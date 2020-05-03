cc01408a31717bdaff9320e329cbc39eaa890faa
uma-shankar-name <umashankargupta7@gmail.com>
2020-05-03 14:12:32 +0530

    first commit

--- blogapp/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PostListView, PostDetailView
-
-urlpatterns = [
-    path('posts/', PostListView.as_view()),
-    path('posts/<int:pk>', PostDetailView.as_view()),
-]
